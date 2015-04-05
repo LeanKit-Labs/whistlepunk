2910fe407fe92ac27c85a3f1a1158e65fcadf547
Alex Robson <asrobson@gmail.com>
2015-04-05 16:52:32 -0500

    Remove specs for previous attempts at timestamp control.

--- spec/behavior/logger.spec.js
+++ spec/behavior/logger.spec.js
@@ -16,19 +16,6 @@ describe( "Logger.js", function() {
 		logger = new (ctorFactory( channelStub ))( "test", [ adapterStub ] );
 	} );
 
-	describe( "when calling logIt", function() {
-		it( "should use a default timestamp if not provided", function() {
-			logger.logIt( "info", [ "Please, I'll use your datetime..." ] );
-			channelStub.publish.firstCall.args[ 1 ].timestamp.should.be.a( "Date" );
-		} );
-		it( "should use a passed timestamp if provided", function() {
-			var myTimestamp = new Date();
-			logger.logIt( "info", [ "Please, use my datetime..." ], myTimestamp );
-			channelStub.publish.firstCall.args[ 1 ].timestamp.should.be.a( "Date" );
-			channelStub.publish.firstCall.args[ 1 ].timestamp.should.equal( myTimestamp );
-		} );
-	} );
-
 	describe( "when calling reset", function() {
 		it( "should unsubscribe all adapters", function() {
 			logger.reset();
